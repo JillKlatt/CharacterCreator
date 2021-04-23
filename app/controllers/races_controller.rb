8e18fc16fd1f3f7811249fec9d9cc66a5aaf8296
JillKlatt <klatt.jill@gmail.com>
2021-04-22 23:00:13 -0400

    Add index, new, and create for Race

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,2 +1,19 @@
 class RacesController < ApplicationRecord
+
+    def index
+        @races = Race.all
+    end
+
+    def new
+        @race = Race.new
+    end
+
+    def create
+        @race = Race.new(race_params)
+            if @race.save
+                redirect_to characters_path
+            else
+                redirect_to :new
+            end
+    end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,5 @@
 class Character < ApplicationRecord
-   self.table_name = "characters"
+   #self.table_name = "characters"
     belongs_to :category
     belongs_to :race
 end
