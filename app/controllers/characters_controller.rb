873a6761c8a70a1d21aebd583a741070f18e5f4b
JillKlatt <klatt.jill@gmail.com>
2021-04-22 22:09:56 -0400

    add create and new and params to character controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,28 @@
 class CharactersController < ApplicationRecord
+
+    def index
+        @characters = current_user.characters
+    end
+
+    def new
+        @character = Character.new
+    end
+
+    def create
+        @character = Character.new(character_params)
+
+        if @character.save
+               redirect_to character_path(@character)
+        else 
+            render :new
+        end
+
+    end
+
+    private
+
+    def character_params
+        params.require(:character).permit(:name, :age, :description, :category_id, :race_id, :category_attributes: [:name, :trait], :race_attributes: [:name, :trait])
+    end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+    def current_user
+        @user ||= User.find_by_id(session[:user_id])
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def readible_datetime(obj)
+        date = obj.created_at.to_date
+
+        date.strftime("%b %d, %Y")
+    end
 end
