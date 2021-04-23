16a45111730e5cb75c6bdf043d9f8ca63491f3fc
JillKlatt <klatt.jill@gmail.com>
2021-04-22 23:15:32 -0400

    Add weapons routes and views

--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -1,3 +1,25 @@
 class WeaponsController < ApplicationRecord
+    
+    def index
+        @weapons = Weapon.all
+    end
 
+    def show
+        @weapon = Weapon.find_by(id: params[:id])
+    end
+
+    def new
+        @weapon = Weapon.new
+    end
+
+    def create
+        @weapon = Weapon.new(weapon_params)
+        redirect_to weapons_path
+    end
+
+    private
+
+    def weapon_params
+        params.require(:weapon).permit(:name, :description, :damage)
+    end
 end
