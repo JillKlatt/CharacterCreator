368bd821082edaf1e990b6c718946dc0af65897b
JillKlatt <klatt.jill@gmail.com>
2021-04-29 15:09:27 -0400

    Change find_dm method to view, cannot work out why it doesn't work as a method

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -14,17 +14,18 @@ class Campaign < ApplicationRecord
 
     def find_dm
         #binding.pry
-        if self.characters.count > 0
+
             self.adventures.each do |adventure|
                 if adventure.role == true
                 @found_character_id = adventure.character_id
+                #byebug
                 end
+                    if !@found_character_id.nil?
+                        @dm = Character.find_by(id: @found_character_id)
+                        return "The DM for this fine game is: #{@dm.name}"
+                    end
             end
-            if !@dm.nil?
-             @dm = Character.find_by(id: @found_character_id)
-                "The DM for this fine game is: #{@dm.name}"
-            end
-        end
+
     end
 
 end
