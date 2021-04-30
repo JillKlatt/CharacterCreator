97e834adbffd88c7dd23e7ea131ab003d0d93010
JillKlatt <klatt.jill@gmail.com>
2021-04-30 10:46:25 -0400

    Add actions to categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,57 @@
 class CategoriesController < ApplicationController
     #display available classes, find or create by new ones
 
+    def index
+        @categories = Category.all
+    end
+
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+            if @category.save
+                redirect_to categories_path
+            else
+                redirect_to :new
+            end
+    end
+
+    def edit                              
+        @category = Category.find_by(id: params[:id])
+    end
+
+    def update
+        @category = Category.find_by(id: params[:id])
+        @category.update(category_params)
+
+  
+        #binding.pry
+
+        if @category.valid?
+            redirect_to categories_path
+        else
+            render :edit
+        end
+    end
+
+    def show
+        @category = Category.find_by(id: params[:id])
+    end
+
+
+    
+    def destroy
+        @category = Category.find_by(id: params[:id])
+        @category.destroy
+        flash[:message] = "Category Deleted"
+        redirect_to categories_path
+    end
+    private
+
+    def category_params
+        params.require(:category).permit(:name, :trait)
+    end
+
 end
