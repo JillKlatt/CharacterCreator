4e873f2aa3fa8bba54d9fe981bfa2c164b164319
JillKlatt <klatt.jill@gmail.com>
2021-05-03 09:10:06 -0400

    Add name validation for weapons and swap login and signup buttons on nav bar

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -2,4 +2,7 @@ class Weapon < ApplicationRecord
     belongs_to :user
     has_many :character_weapons
     has_many :characters, through: :character_weapons
+
+    validates :name, presence: true
+
 end
