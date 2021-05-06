ec0d9a73c207366ca721d5b14fd9cf487e7ede64
JillKlatt <klatt.jill@gmail.com>
2021-05-05 22:08:22 -0400

    Add styling to hopefully make buttons more readable

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,5 +1,8 @@
 class CampaignsController < ApplicationController
     before_action :redirect_if_not_logged_in
+    before_action :set_campaign, only: [:show, :edit, :update, :destroy]
+    before_action :destroy_adventures, only: [:destroy]
+    before_action :set_characters, only: [:new, :create, :edit, :update]
 
     def show
         @campaign = Campaign.find_by(id: params[:id])
@@ -50,11 +53,35 @@ class CampaignsController < ApplicationController
         end
     end
 
+    def destroy
+        @campaign.destroy
+            if @campaign.destroy
+                flash[:message] = "Campaigns Deleted"
+                redirect_to campaigns_path
+            else
+                flash[:message] = @campaign.errors.full_messages.join(" ")
+            end
+    end
+
 
     private
 
     def campaign_params
         params.require(:campaign).permit(:name, :schedule, :description, :user_id, :id, :adventure_admin, character_ids: [], character_attributes: [:name, :age, :race, :category, :description])
+    end
+
+    def destroy_adventures
+        @campaign.adventures.each do |adventure|
+            adventure.destroy
+        end
+    end
+
+    def set_campaign
+        @campaign = Campaign.find_by(id: params[:id])
+    end
+
+    def set_characters
+        @characters = current_user.characters
     end
 
     
