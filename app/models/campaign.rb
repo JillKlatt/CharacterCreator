10fac48951cc25aa2a4b16e030cd1b68a4863169
JillKlatt <klatt.jill@gmail.com>
2021-05-07 20:26:30 -0400

    Add validation to test

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,6 +1,7 @@
 class Campaign < ApplicationRecord
     has_many :adventures
     has_many :characters, through: :adventures
+    validates :name, presence: true
     #belongs_to :user, optional: true
 
     # accepts_nested_attributes_for :characters
